7041163d198cf09b65d9256224b56d8e1159fe8f
marz_fd <mar.31069@gmail.com>
2023-07-19 15:34:04 +0330

    feat: add eslint and stylelint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'resume-website'`, () => {
+  it("should have as title 'resume-website'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('resume-website');
