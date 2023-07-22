3a9d17004a1ca2f9c098a88e81280b7e43509037
marz_fd <mar.31069@gmail.com>
2023-07-22 16:32:45 +0330

    chore: change settings for eslint, stylelinet and unit testing

--- src/jest.config.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['src'],
-  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  transformIgnorePatterns: ['node_modules/(?!(jest-test))'],
-  testEnvironment: 'node',
-};
