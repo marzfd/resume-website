86b90427e2bc91ac233975efd5e200e1802b8f19
Marzieh <mar.31069@gmail.com>
2023-08-17 09:22:18 +0330

    refactor(app): rename a file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 
