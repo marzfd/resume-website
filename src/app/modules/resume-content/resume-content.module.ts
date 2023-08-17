d48af89dc9390e3bd75cad0f923b3c7a63a9ed9c
Marzieh <mar.31069@gmail.com>
2023-08-17 19:25:13 +0330

    feat(app): add first page and change header style

--- src/app/modules/resume-content/resume-content.module.ts
+++ src/app/modules/resume-content/resume-content.module.ts
@@ -6,9 +6,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatLegacyListModule } from '@angular/material/legacy-list';
 import { HeaderModule } from '../header/header.module';
+import { HomeComponent } from '../home/home.component';
 
 @NgModule({
-  declarations: [ResumeContentComponent],
+  declarations: [ResumeContentComponent, HomeComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
