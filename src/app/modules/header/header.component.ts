1818495222da706cab301e0f6d9e50fda275676c
marz_fd <mar.31069@gmail.com>
2023-08-19 15:52:57 +0330

    refactor(header): use for loop for navigation buttons

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  navigationLinks = [
+    { label: 'Experiences', route: '/experiences' },
+    { label: 'Educations', route: '/educations' },
+    { label: 'Projects', route: '/projects' },
+    { label: 'Skills', route: '/skills' },
+  ];
 }
